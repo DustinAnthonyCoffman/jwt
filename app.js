82e39edc32a10af23ea2aafb196f16c9617c82c7
“Dustin <dustin@inleague.org>
2022-12-06 17:53:21 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Dustin:qmk22lpnEkmubKSb@cluster0.neknura.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
