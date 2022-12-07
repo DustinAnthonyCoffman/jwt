54255d3d8cefb11c985c21cbafb58f72fcf583e4
“Dustin <dustin@inleague.org>
2022-12-06 23:57:12 -0600

    adds jwt and cookie in user create, routes user at signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
@@ -17,6 +18,14 @@ const handleErrors = (err) => {
     }
     return errors;
 }
+//3 days
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+    return jwt.sign({ id }, 'my super secret', {
+        expiresIn: maxAge
+    })
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -31,7 +40,9 @@ module.exports.signup_post = async (req, res) => {
 
     try {
         const user = await User.create({email, password});
-        res.status(201).json(user)
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(201).json({user: user._id})
     }
     catch(err) {
         const errors = handleErrors(err);
