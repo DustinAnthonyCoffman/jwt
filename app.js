97a6dd628b9654bc2bb52dc428a6f25efa6f383f
“Dustin <dustin@inleague.org>
2022-12-07 01:44:33 -0600

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb+srv://Dustin:qmk22lpnEkmubKSb@cluster0.neknura.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(process.env.PORT || 3000))
   .catch((err) => console.log(err));
 
 // routes
